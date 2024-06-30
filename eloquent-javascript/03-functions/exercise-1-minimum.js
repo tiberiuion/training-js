652cce75d230024102a3712d39a79005bdaba75c
Tiberiu Ion <tiberiu.vi@gmail.com>
2024-06-30 23:02:44 +0100

    Exercise 1 minimum - start

---
+++ eloquent-javascript/03-functions/exercise-1-minimum.js
@@ -0,0 +1,10 @@
+// Minimum
+
+// The previous chapter introduced the standard function Math.min that returns its smallest argument. We can write a function like that ourselves now. Define the function min that takes two arguments and returns their minimum.
+
+// Your code here.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
