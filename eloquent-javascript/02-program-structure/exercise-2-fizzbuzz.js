5939707fdaf35e2f6afb860d1608fe1153d700b3
Tiberiu Ion <tiberiu.vi@gmail.com>
2026-03-27 15:45:50 +0000

    Refactor capitalisation to match requirements.

--- eloquent-javascript/02-program-structure/exercise-2-fizzbuzz.js
+++ eloquent-javascript/02-program-structure/exercise-2-fizzbuzz.js
@@ -10,11 +10,11 @@
 
 for (let i = 1; i <= 100; i++) {
     if (i % 5 == 0 && i % 3 == 0) {
-        console.log('fizzbuzz');
+        console.log('FizzBuzz');
     } else if (i % 3 == 0) {
-        console.log('fizz')
+        console.log('Fizz')
     } else if (i % 5 == 0) {
-        console.log('buzz');
+        console.log('Buzz');
     } else {
         console.log(i);
     }
